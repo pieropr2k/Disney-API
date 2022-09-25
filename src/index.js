d6a6887118273811557e95fd52e8f7251214b1fd
Piero <piero.pilco.r@uni.pe>
2022-09-25 12:35:31 -0500

    Added command start

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const app = require("./app.js");
-require('dotenv').config()
 const sequelize = require("./database/database.js");
+const { DB_PORT } = require("./utils/config.js");
 require('./models/character.js')
 require('./models/serie.js')
+const PORT = DB_PORT || 4000
 
 const main = async () => {
     try {
         await sequelize.sync({ force: false });
         //.sync({ alter: true })
         //await sequelize.authenticate();
-        app.listen(process.env.DB_PORT);
+        app.listen(PORT);
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
